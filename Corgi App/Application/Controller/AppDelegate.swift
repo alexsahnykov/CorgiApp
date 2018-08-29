4c090ad3f2ee872a57ef31f2b4721c00ecdb9087
Alexander <alexsahnykov@gmail.com>
2018-08-29 15:29:15 +0300

    firebaseinit

--- Corgi App/Application/Controller/AppDelegate.swift
+++ Corgi App/Application/Controller/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ static var menu_bool = true
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UIApplication.shared.statusBarStyle = .lightContent
+        FirebaseApp.configure()
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Corgi App' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+
+
+
+  # Pods for Corgi App
+
+  target 'Corgi AppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Corgi AppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
